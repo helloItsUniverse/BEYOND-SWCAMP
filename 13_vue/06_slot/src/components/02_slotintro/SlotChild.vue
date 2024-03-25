6721b6106b1dc63e7878534cfd06707799513f50
universe <hello.its.universe@gmail.com>
2024-03-25 17:13:00 +0900

    vue CLI

---
+++ 13_vue/06_slot/src/components/02_slotintro/SlotChild.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1>
+            Child가 받은 slot => <slot></slot>
+        </h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ 13_vue/06_slot/src/components/03_slotnamed/NamedChild.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <slot name="name"></slot>
+        <slot name="founder"></slot>
+        <slot name="img"></slot>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
